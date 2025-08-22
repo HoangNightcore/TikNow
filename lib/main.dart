fac53e22cda9cf47468c15e12f4427c91350e8ae
HoangNightcore <2001215792@hufi.edu.vn>
2025-08-22 12:01:15 +0700

    Add some dependences

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:tix_now/app.dart';
 import 'package:tix_now/core/services/data_seed_service.dart';
 import 'package:tix_now/features/auth/presentation/pages/login_page.dart';
-import 'package:tix_now/features/auth/presentation/pages/sign_in_screen.dart';
+import 'package:tix_now/features/auth/presentation/pages/login_page.dart';
 import 'package:tix_now/features/auth/presentation/pages/splash_screen.dart';
 
 void main() async {
